c878701f9f35866f1f9cc373c56aadea5b06129a
fuhoang <fuhoang84@googlemail.com>
2016-05-17 22:43:41 +0100

    category seeder class created

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'name' => 'uncategorized',
+            'description' => 'Some description about the category goes here.',
+        ]);
+    }
+}
