df2df45ed5776ceaa04679a48ee0df9b1649c00e
Fu Hoang <fu.hoang@creativevirtual.com>
2016-05-19 16:12:40 +0100

    transform data and added http status code

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Response;
 use App\Http\Requests;
 use App\Category;
 use App\User;
@@ -37,12 +38,42 @@ class VideosController extends Controller
         if($request->get('search')){
             $videos = Video::where("title", "LIKE", "%{$request->get('search')}%")->paginate(5);
         }else{
-            $videos = Video::with('user')->paginate(5);
+            $videos = Video::with('user')->paginate(20);
         }
 
-        return response($videos);
+        //return response($videos);
+
+
+        return Response::json([
+            'videos' => $this->transformCollection($videos)
+            //'data' => $videos
+        ], 200);
 
     }
+
+    /**
+     * Show all videos
+     *
+     * @return Response
+     */
+    public function show($id){
+        $video = Video::with('user')->find($id);
+        //return response($video);
+        if(!$video){
+            return Response::json([
+                'error' => [
+                    'message' => 'Video does not exist'
+                    ]
+            ], 404);
+        }
+
+        return Response::json([
+            'video' => $this->transform($video)
+        ], 200);
+
+    }
+
+
 
     /**
      * Creating a new post
@@ -54,8 +85,12 @@ class VideosController extends Controller
         $create = Auth::user()->videos()->create($request->all());
         $user = User::find($create->user_id);
         $create->user = $user;
+        //return response($create);
 
-        return response($create);
+        return Response::json([
+            'message' => 'Video Created Successfully',
+            'videos' => $this->transform($create)
+        ], 200);
 
     }
 
@@ -70,8 +105,14 @@ class VideosController extends Controller
     {
         $video = Video::find($id);
         $cat = Category::find($video->category_id);
-        $video->category = $cat;
-        return response($video);
+        //$video->category = $cat;
+
+        return Response::json([
+            'video' => $this->transform($video, $cat)
+            //'category'=> $cat
+        ], 200);
+
+
     }
 
 
@@ -83,12 +124,14 @@ class VideosController extends Controller
      * @return Response
      */
     public function update(Request $request, $id){
-        $input = $request->all();
-        Video::where("id", $id)->update($input);
-        $video = Video::find($id);
-        $user = User::find($video->user_id);
-        $video->user = $user;
-        return response($video);
+
+        Video::where("id", $id)->update($request->all());
+        $video = Video::with('user')->find($id);
+        return Response::json([
+            'message' => 'Video Updated Successfully',
+            'video' => $this->transform($video)
+        ], 200);
+
     }
 
 
@@ -102,4 +145,38 @@ class VideosController extends Controller
         return Video::where('id', $id)->delete();
     }
 
+
+    private function transformCollection($videos){
+
+        $videosArray = $videos->toArray();
+
+        return array_map([$this, 'transform'], $videosArray['data']);
+    }
+
+    private function transform($video, $category = null){
+
+        $transform = [
+            'id' => $video['id'],
+            'title' => $video['title'],
+            'description' => $video['description'],
+            'video_link' => $video['video_link'],
+            'user' => [
+                'id' => $video['user']['id'],
+                'created_by' => $video['user']['name'],
+                'email' => $video['user']['email']
+            ]
+        ];
+
+        if($category != null){
+            $transform['category'] = $category;
+
+        }
+
+
+
+
+
+
+        return $transform;
+    }
 }
--- public/app/controllers/VideoController.js
+++ public/app/controllers/VideoController.js
@@ -29,7 +29,7 @@ app.controller('VideoController', function(dataFactory, $scope, $http){
         }else{
 
             dataFactory.httpRequest('api/v1/videos?page='+pageNumber).then(function(data) {
-                $scope.data = data.data;
+                $scope.data = data.videos;
                 $scope.totalItems = data.total;
             });
         }
@@ -40,8 +40,8 @@ app.controller('VideoController', function(dataFactory, $scope, $http){
         if(valid){
             $scope.video.category_id = $scope.cat_id.id;
             dataFactory.httpRequest('api/v1/videos','POST',{},$scope.video).then(function(data) {
-                console.log(data);
-                $scope.data.push(data);
+                console.log(data.videos);
+                $scope.data.push(data.videos);
                 $(".modal").modal("hide");
             });
         }else{
@@ -52,23 +52,22 @@ app.controller('VideoController', function(dataFactory, $scope, $http){
     $scope.edit = function(id){
 
         dataFactory.httpRequest('api/v1/videos/'+id+'/edit').then(function(data) {
-            console.log(data);
-            console.log(data.category);
-            $scope.cat_id = data.category;
-            $scope.editForm = data;
+            $scope.cat_id = data.video.category;
+            $scope.editForm = data.video;
         });
     }
 
     $scope.saveEdit = function(valid){
 
         if(valid){
-            delete $scope.editForm.category;
+            delete $scope.editForm.category
+            delete $scope.editForm.user;
             $scope.editForm.category_id = $scope.cat_id.id;
+
             dataFactory.httpRequest('api/v1/videos/'+$scope.editForm.id,'PUT',{},$scope.editForm).then(function(data) {
                 angular.forEach($scope.data, function(video, key) {
-                    console.log(key);
-                    if(video.id == data.id){
-                        $scope.data[key] = data;
+                    if(video.id == data.video.id){
+                        $scope.data[key] = data.video;
                     }
                 });
                 $(".modal").modal("hide");
